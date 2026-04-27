a427ab043245c34336a522195c29dac697e0a31d
larrylalong-dev <larrylalong20@gmail.com>
2026-04-27 19:51:43 -0400

    first commit

--- backend_jallonII/src/index.js
+++ backend_jallonII/src/index.js
@@ -9,6 +9,7 @@ const reportRoutes = require('./routes/reportRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
@@ -31,6 +32,6 @@ app.use((err, req, res, next) => {
     res.status(500).json({ erreur: 'Erreur interne du serveur.' });
 });
 
-app.listen(PORT, () => {
-    console.log(`CitoyenActif API démarrée sur le port ${PORT}`);
+app.listen(PORT, HOST, () => {
+    console.log(`CitoyenActif API démarrée sur http://${HOST}:${PORT}`);
 });
